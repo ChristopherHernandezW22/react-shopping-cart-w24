7bc28a9bbfb5427eb8ddba7658375a8efe2fb658
Christopher Hernandez <cerberean661@gmail.com>
2019-11-04 16:10:34 -0800

    fixed error by changing props to new value in Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Product from './Product';
 
+import ProductContext from '../contexts/ProductContext';
+
 const Products = props => {
+	// capturing output. Will give us back the value object from App.js from the .provider
+	const value = useContext(ProductContext)
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{value.products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={value.addItem}
 				/>
 			))}
 		</div>
