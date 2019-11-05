b2ab759582a375708a862fb096f14b44889a16bd
Christopher Hernandez <cerberean661@gmail.com>
2019-11-04 16:13:51 -0800

    step 4 complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,14 +7,14 @@ import ProductContext from '../contexts/ProductContext';
 
 const Products = props => {
 	// capturing output. Will give us back the value object from App.js from the .provider
-	const value = useContext(ProductContext)
+	const { products, addItem } = useContext(ProductContext)
 	return (
 		<div className="products-container">
-			{value.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={value.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
